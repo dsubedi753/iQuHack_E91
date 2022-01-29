7d8d0c1fcb639fd2d7f0ba9ba6f6aec3d376f99e
Divas Subedi <divas.subedi@gmail.com>
2022-01-29 14:40:19 -0500

    Server-Client Connection

---
+++ server_socket.py
@@ -0,0 +1,42 @@
+import socket
+import os
+from _thread import *
+
+
+def get_reply(data):
+    # Stubb function for reply
+    return 'Server Says: ' + data.decode('utf-8')
+
+ServerSocket = socket.socket()
+host = '127.0.0.1'
+port = 1233
+ThreadCount = 0
+try:
+    ServerSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+print('Waitiing for a Connection..')
+ServerSocket.listen(5)
+
+
+def threaded_client(connection):
+    connection.send(str.encode('Welcome to the Server'))
+    while True:
+        data = connection.recv(2048)
+        reply = get_reply(data)
+        if not data:
+            break
+        connection.sendall(str.encode(reply))
+    connection.close()
+
+while True:
+    Client, address = ServerSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSocket.close()
+
+
+    
