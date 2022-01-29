7105f049ea96445a289b2014f0892ab6c46f6be7
Paul Hermes <paulhermes.ph@gmail.com>
2022-01-29 19:22:33 +0100

    MWP for E91 protocol

--- protocol.py
+++ protocol.py
@@ -1,5 +1,4 @@
 import random
-import numpy as np
 
 
 def establish_connection():  # True = Adam, False = Bob
@@ -14,33 +13,47 @@ def q_receive_result():
     pass
 
 
-def c_send_basis(basis_string):
+def c_send_basis(basis_arr):
     pass
 
 
 def c_receive_basis():
-    return "01101010100011011010"
+    return []
 
 
-def e91protocol(bit_string_length, seed, rand_gen, error):
+def c_receive_decoy():
+    return []
+
+
+def c_send_decoy(decoy):
+    pass
+
+
+def e91protocol(bit_string_length, seed, rand_gen):
     role = establish_connection()
     rand_gen.seed(seed)
     basis_arr = []
     results_arr = []
     for _ in range(bit_string_length):
-        basis_arr.append(rand_gen.choice([0, 1, 2] if role == "adam" else [1, 2, 3]))
+        basis_arr.append(rand_gen.choice([0, 1, 2] if role else [1, 2, 3]))
         q_send_basis(basis_arr[-1])
         results_arr.append(q_receive_result())
-    c_send_basis(basis_string)
-    other_basis_string = c_receive_basis()
+    c_send_basis(basis_arr)
+    other_basis_arr = c_receive_basis()
     key = []
-    s = 0
-    for b in zip(basis_string, other_basis_string):
-        if b[0] == b[0]:
-            key.append(results_arr)
+    decoy = []
+    for (basis, other, result) in zip(basis_arr, other_basis_arr, results_arr):
+        if basis == other:
+            key.append(result)
         else:
-            pass
-
+            decoy.append(result)
+    c_send_decoy(decoy)
+    other_decoy = c_receive_decoy()
+    s = 0
+    for d in zip(decoy, other_decoy):
+        s += (1 if d[0] == d[1] else -1)
+    s = s/len(decoy)
+    return s, key
 
 
 if __name__ == "__main__":
