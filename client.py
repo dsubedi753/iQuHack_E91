a0e82908b9f931bc13cd848e9384eb4f58cd9038
Divas Subedi <divas.subedi@gmail.com>
2022-01-29 17:04:29 -0500

    Server_client update

--- client.py
+++ client.py
@@ -1,20 +1,112 @@
-import socket
+import socket, pickle
+import random
 
-ClientSocket = socket.socket()
-host = '127.0.0.1'
-port = 1233
 
-print('Waiting for connection')
-try:
-    ClientSocket.connect((host, port))
-except socket.error as e:
-    print(str(e))
 
-Response = ClientSocket.recv(1024)
-while True:
-    Input = input('Say Something: ')
-    ClientSocket.send(str.encode(Input))
+def establish_connection():  # True = Adam, False = Bob
+    print('Waiting for connection')
+    try:
+        ClientSocket.connect((host, port))
+    except socket.error as e:
+        print(str(e))
+    # Recieve the Client number
+    cl_num = int(ClientSocket.recv(1024).decode('utf-8'))
+    print("Welcome " + ("Adam" if cl_num == 1 else "Bob"))
+    return cl_num == 1
+
+
+def q_send_basis(basis):
+    # Pickles the array and sends to int server
+    ClientSocket.send(str.encode(str(basis)))
+
+
+
+def q_receive_result():
     Response = ClientSocket.recv(1024)
     print(Response.decode('utf-8'))
+    return int(Response.decode('utf-8'))
 
-ClientSocket.close()
+
+def c_send_basis(basis_arr):
+    # PSocket.send(pickle.dumps(basis_arr))))
+    pass
+
+
+def c_receive_basis():
+    # data = PSocket.recv(4096)
+    # basis = pickle.loads(data)
+    return []
+
+
+def c_receive_decoy():
+    return []
+
+
+def c_send_decoy(decoy):
+    pass
+
+
+def p2p_server():
+    pass
+
+def p2p_client():
+    pass
+
+
+def e91protocol(bit_string_length, seed, rand_gen):
+    role = establish_connection()
+    
+    if role:
+        p2p_server()
+    else:
+        p2p_client()
+    
+    rand_gen.seed(seed)
+    basis_arr = []
+    results_arr = []
+    for _ in range(bit_string_length):
+        basis_arr.append(rand_gen.choice([0, 1, 2] if role else [1, 2, 3]))
+        q_send_basis(basis_arr[-1])
+        results_arr.append(q_receive_result())
+    c_send_basis(basis_arr)
+    other_basis_arr = c_receive_basis()
+    key = []
+    decoy = []
+    for (basis, other, result) in zip(basis_arr, other_basis_arr, results_arr):
+        if basis == other:
+            key.append(result)
+        else:
+            decoy.append(result)
+    c_send_decoy(decoy)
+    other_decoy = c_receive_decoy()
+    s = 0
+    for d in zip(decoy, other_decoy):
+        s += (1 if d[0] == d[1] else -1)
+    s = s/len(decoy)
+    return s, key
+
+
+
+if __name__ == "__main__":
+    ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '127.0.0.1'
+    port = 1233
+    
+    PSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host_p = '127.0.0.1'
+    port_p = 1234
+    
+    establish_connection()
+    q_send_basis(1)
+    q_receive_result()
+    
+    # while True:
+    #     Input = input('Say Something: ')
+    #     ClientSocket.send(str.encode(Input))
+    #     Response = ClientSocket.recv(1024)
+    #     print(Response.decode('utf-8'))
+
+    #e91protocol(20, 103942, random)
+    
+    ClientSocket.close()
+
--- server_socket.py
+++ server_socket.py
@@ -1,42 +1,54 @@
-import socket
+import socket, pickle
 import os
 from _thread import *
+import random
 
 
 def get_reply(data):
     # Stubb function for reply
-    return 'Server Says: ' + data.decode('utf-8')
+    basis = pickle.loads(data)
+    print(repr(basis))
+    return 'Server Says: Hello'
 
-ServerSocket = socket.socket()
-host = '127.0.0.1'
-port = 1233
-ThreadCount = 0
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
-
-print('Waitiing for a Connection..')
-ServerSocket.listen(5)
+def get_result(basis):
+    # Stubb function for result
+    print(basis.decode('utf-8'))
+    return str(random.choice([0,1]))
 
 
-def threaded_client(connection):
-    connection.send(str.encode('Welcome to the Server'))
+def threaded_client(connection, cl_num):
+    connection.send(str.encode(str(cl_num)))
     while True:
-        data = connection.recv(2048)
-        reply = get_reply(data)
+        data = connection.recv(1024)
         if not data:
             break
+        reply = get_result(data)
         connection.sendall(str.encode(reply))
     connection.close()
 
-while True:
-    Client, address = ServerSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(threaded_client, (Client, ))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-ServerSocket.close()
+
+if __name__ == "__main__":
+    ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '127.0.0.1'
+    port = 1233
+    ThreadCount = 0
+    try:
+        ServerSocket.bind((host, port))
+    except socket.error as e:
+        print(str(e))
+    
+    print('Waitiing for a Connection..')
+    ServerSocket.listen(5)
+    
+    
+    
+    while True:
+        Client, address = ServerSocket.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+        ThreadCount += 1
+        start_new_thread(threaded_client, (Client,ThreadCount, ))
+        print('Thread Number: ' + str(ThreadCount))
+    ServerSocket.close()
 
 
     
