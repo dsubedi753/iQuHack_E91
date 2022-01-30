e409f526d25807498351858e560d9ecd47daa107
Isamade <52768347+Isamade@users.noreply.github.com>
2022-01-30 10:08:36 +0100

    Created quantum inspire function

--- quantum_inspire.py
+++ quantum_inspire.py
@@ -17,34 +17,35 @@ authentication = get_authentication()
 QI.set_authentication(authentication, QI_URL, project_name=project_name)
 qi_backend = QI.get_backend('QX single-node simulator')
 
-alice_bases = '1111111111'
-bob_bases = '1111111111'
-alice_measure = []
-bob_measure = []
+def run_qi(alice_bases, bob_bases)
+    alice_measure = []
+    bob_measure = []
 
-for i in range(len(alice_bases)):
-    q = QuantumRegister(2)
-    b = ClassicalRegister(2)
-    circuit = QuantumCircuit(q, b)
+    for i in range(len(alice_bases)):
+        q = QuantumRegister(2)
+        b = ClassicalRegister(2)
+        circuit = QuantumCircuit(q, b)
 
-    circuit.h(q[0])
-    circuit.cx(q[0], q[1])
-    circuit.x(q[1])
-    
-    if alice_bases[i] == '0':
-        circuit.ry(-pi/2,q[0])
-    elif alice_bases[i] == '1':
-        circuit.ry(-pi/4,q[0])
-    if bob_bases[i] == '1':
-        circuit.ry(-pi/4,q[1])
-    elif bob_bases[i] == '3':
-        circuit.ry(pi/4,q[1])
-        
-    #circuit.measure(q[0], b[0])
-    #circuit.measure(q[1], b[1])
+        circuit.h(q[0])
+        circuit.cx(q[0], q[1])
+        #circuit.x(q[1])
 
-    job = execute(circuit, backend=qi_backend, shots=1)
-    result = job.result().get_counts(circuit)
-    for state, counts in result.items():
-        alice_measure.append(state[1])
-        bob_measure.append(state[0])
+        if alice_bases[i] == 0:
+            circuit.ry(-pi/2,q[0])
+        elif alice_bases[i] == 1:
+            circuit.ry(-pi/4,q[0])
+        if bob_bases[i] == 1:
+            circuit.ry(-pi/4,q[1])
+        elif bob_bases[i] == 3:
+            circuit.ry(pi/4,q[1])
+
+        #circuit.measure(q[0], b[0])
+        #circuit.measure(q[1], b[1])
+
+        job = execute(circuit, backend=qi_backend, shots=1)
+        result = job.result().get_counts(circuit)
+        for state, counts in result.items():
+            alice_measure.append(state[1])
+            bob_measure.append(state[0])
+
+        return alice_measure, bob_measure
