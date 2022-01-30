8d04979c97ac952c9c60d70c6d32f9ed6c742d61
Paul Hermes <paulhermes.ph@gmail.com>
2022-01-30 14:14:13 +0100

    went back to 01 and 10 and did some refactoring to adhear to pip

--- quantum_inspire.py
+++ quantum_inspire.py
@@ -1,14 +1,13 @@
-import os
-from math import pi
-
-from quantuminspire.credentials import enable_account
-enable_account('YOUR_API_TOKEN')
-
 from qiskit import execute
 from qiskit.circuit import QuantumRegister, ClassicalRegister, QuantumCircuit
-
 from quantuminspire.credentials import get_authentication
 from quantuminspire.qiskit import QI
+import os
+from math import pi
+from quantuminspire.credentials import enable_account
+
+
+enable_account('YOUR_API_TOKEN')
 
 QI_URL = os.getenv('API_URL', 'https://api.quantum-inspire.com/')
 
@@ -17,26 +16,22 @@ authentication = get_authentication()
 QI.set_authentication(authentication, QI_URL, project_name=project_name)
 qi_backend = QI.get_backend('QX single-node simulator')
 
-def run_qi(alice_bases, bob_bases):
-    alice_measure = []
-    bob_measure = []
 
-    for i in range(len(alice_bases)):
+def run_qi(bases_0, bases_1):
+    measure_0 = []
+    measure_1 = []
+
+    for base_zero, base_one in zip(bases_0, bases_1):
         q = QuantumRegister(2)
         b = ClassicalRegister(2)
         circuit = QuantumCircuit(q, b)
 
         circuit.h(q[0])
+        circuit.x(q[1])
         circuit.cx(q[0], q[1])
 
-        if alice_bases[i] == 0:
-            circuit.ry(-pi/2,q[0])
-        elif alice_bases[i] == 1:
-            circuit.ry(-pi/4,q[0])
-        if bob_bases[i] == 1:
-            circuit.ry(-pi/4,q[1])
-        elif bob_bases[i] == 3:
-            circuit.ry(pi/4,q[1])
+        circuit.ry(base_zero*pi/4, q[0])
+        circuit.ry(base_one*pi/4, q[1])
 
         circuit.measure(q[0], b[0])
         circuit.measure(q[1], b[1])
@@ -44,7 +39,7 @@ def run_qi(alice_bases, bob_bases):
         job = execute(circuit, backend=qi_backend, shots=1)
         result = job.result().get_counts(circuit)
         for state, counts in result.items():
-            alice_measure.append(int(state[1]))
-            bob_measure.append(int(state[0]))
+            measure_0.append(int(state[0]))
+            measure_1.append(int(state[1]))
 
-    return alice_measure, bob_measure
+    return measure_0, measure_1
